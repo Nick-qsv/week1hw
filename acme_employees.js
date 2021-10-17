3aaca757cf729cfafcc6ddebe74fe05c1bb169bf
Nick Baez <w0m85k27@protonmail.com>
2021-10-17 19:49:53 -0400

    all problems done except last

--- acme_employees.js
+++ acme_employees.js
@@ -1,60 +1,91 @@
 const employees = [
-    { id: 1, name: 'moe'},
-    { id: 2, name: 'larry', managerId: 1},
-    { id: 4, name: 'shep', managerId: 2},
-    { id: 3, name: 'curly', managerId: 1},
-    { id: 5, name: 'groucho', managerId: 3},
-    { id: 6, name: 'harpo', managerId: 5},
-    { id: 8, name: 'shep Jr.', managerId: 4},
-    { id: 99, name: 'lucy', managerId: 1}
-  ];
-  
-  const spacer = (text)=> {
-    if(!text){
-      return console.log('');
-    }
-    const stars = new Array(5).fill('*').join('');
-    console.log(`${stars} ${text} ${stars}`);
+  { id: 1, name: "moe" },
+  { id: 2, name: "larry", managerId: 1 },
+  { id: 4, name: "shep", managerId: 2 },
+  { id: 3, name: "curly", managerId: 1 },
+  { id: 5, name: "groucho", managerId: 3 },
+  { id: 6, name: "harpo", managerId: 5 },
+  { id: 8, name: "shep Jr.", managerId: 4 },
+  { id: 99, name: "lucy", managerId: 1 },
+];
+
+const spacer = (text) => {
+  if (!text) {
+    return console.log("");
   }
-  
-  spacer('findEmployeeByName Moe')
-  // given a name and array of employees, return employee
-  console.log(findEmployeeByName('moe', employees));//{ id: 1, name: 'moe' }
-  spacer('')
-  
-  spacer('findManagerFor Shep Jr.')
-  //given an employee and a list of employees, return the employee who is the manager
-  console.log(findManagerFor(findEmployeeByName('shep Jr.', employees), employees));//{ id: 4, name: 'shep', managerId: 2 }
-  spacer('')
-  
-  spacer('findCoworkersFor Larry')
-  
-  //given an employee and a list of employees, return the employees who report to the same manager
-  console.log(findCoworkersFor(findEmployeeByName('larry', employees), employees));/*
+  const stars = new Array(5).fill("*").join("");
+  console.log(`${stars} ${text} ${stars}`);
+};
+
+spacer("findEmployeeByName Moe");
+function findEmployeeByName(employeeName, arr) {
+  //{ id: 1, name: "moe" },
+  return arr.filter((element) => element.name === employeeName)[0];
+  //   const filteredArr = arr.filter((element) => element.name === employeeName)[0]
+  //     console.log('RESULT---->', filteredArr)
+  //   return `{${filteredArr}}`;
+}
+// given a name and array of employees, return employee
+console.log(findEmployeeByName("moe", employees)); //{ id: 1, name: 'moe' }
+spacer("");
+
+spacer("findManagerFor Shep Jr.");
+function findManagerFor(obj, arr) {
+  return arr.filter((element) => element.id === obj.managerId)[0];
+}
+//given an employee and a list of employees, return the employee who is the manager
+console.log(
+  findManagerFor(findEmployeeByName("shep Jr.", employees), employees)
+); //{ id: 4, name: 'shep', managerId: 2 }
+spacer("");
+
+spacer("findCoworkersFor Larry");
+function findCoworkersFor(obj, arr){
+//     //assuming larry is input, you get curly and lucy output
+
+return arr.filter((element) => element.managerId === obj.managerId && element.name !== obj.name)
+}
+//given an employee and a list of employees, return the employees who report to the same manager
+console.log(
+  findCoworkersFor(findEmployeeByName("larry", employees), employees)
+); /*
   [ { id: 3, name: 'curly', managerId: 1 },
     { id: 99, name: 'lucy', managerId: 1 } ]
   */
-  
-  spacer('');
-  
-  spacer('findManagementChain for moe')
-  //given an employee and a list of employees, return a the management chain for that employee. The management chain starts from the employee with no manager with the passed in employees manager 
-  console.log(findManagementChainForEmployee(findEmployeeByName('moe', employees), employees));//[  ]
-  spacer('');
-  
-  spacer('findManagementChain for shep Jr.')
-  console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees));/*
+
+spacer("");
+
+spacer("findManagementChain for moe");
+function findManagementChainForEmployee(obj, arr){
+    //so return all of the managerID numbers higher than your manager ID number
+    return arr.filter((element) => element.id <= obj.managerId)
+}
+//given an employee and a list of employees, return a the management chain for that employee. The management chain starts from the employee with no manager with the passed in employees manager
+console.log(
+  findManagementChainForEmployee(
+    findEmployeeByName("moe", employees),
+    employees
+  )
+); //[  ]
+spacer("");
+
+spacer("findManagementChain for shep Jr.");
+console.log(
+  findManagementChainForEmployee(
+    findEmployeeByName("shep Jr.", employees),
+    employees
+  )
+); /*
   [ { id: 1, name: 'moe' },
     { id: 2, name: 'larry', managerId: 1 },
     { id: 4, name: 'shep', managerId: 2 }]
   */
-  spacer('');
-  
-  
-  spacer('generateManagementTree')
-  //given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
-  console.log(JSON.stringify(generateManagementTree(employees), null, 2));
-  /*
+spacer("");
+
+spacer("generateManagementTree");
+//given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
+console.log(JSON.stringify(generateManagementTree(employees), null, 2));
+/*
   {
     "id": 1,
     "name": "moe",
@@ -108,11 +139,11 @@ const employees = [
     ]
   }
   */
-  spacer('');
-  
-  spacer('displayManagementTree')
-  //given a tree of employees, generate a display which displays the hierarchy
-  displayManagementTree(generateManagementTree(employees));/*
+spacer("");
+
+spacer("displayManagementTree");
+//given a tree of employees, generate a display which displays the hierarchy
+displayManagementTree(generateManagementTree(employees)); /*
   moe
   -larry
   --shep
